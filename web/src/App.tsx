5974192bc793135f7915ba5f3e16a60f16a58e7d
Wagner Mattei <wagner.mattei@gmail.com>
2021-11-01 01:36:08 -0300

    feat: basic configuration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    window.TrelloPowerUp.initialize({
+      'card-buttons': () => {
+        return [{
+          icon: 'https://cdn.trello.com/1/templates/f0d8b8c4.svg',
+          text: 'Dependencies',
+        }]
+      }
+    })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello world</h1>
   );
 }
 
---
+++ web/src/typings.d.ts
@@ -0,0 +1,5 @@
+interface Window {
+  TrelloPowerUp: {
+    initialize: (options: Record<string, any>) => void;
+  };
+}
